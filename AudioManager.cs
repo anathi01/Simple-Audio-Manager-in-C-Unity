8e99b6f8939241cbee1d340956d112cf7895af07
Anathi <82667935+anathi01@users.noreply.github.com>
2024-01-06 15:22:07 +0200

    Add files via upload

---
+++ AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public GameObject[] audio;
+	
+	//void Update()
+		//dont destroy on load
+	
+	public void PlayAudio(int index){
+		if (audio[index] != null){
+			audio[index].GetComponent<AudioSource>().Play();
+		}else{
+			return;
+		}
+	}
+	
+	public void StopAudio(int index){
+		if (audio[index] != null){
+			audio[index].GetComponent<AudioSource>().Stop();
+		}else{
+			return;
+		}
+	}
+}
